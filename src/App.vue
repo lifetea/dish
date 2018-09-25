1e9231df8b5cb0820d8bcf2648bce94aaaf9c1d3
lifetea <494886251@qq.com>
2018-09-25 09:00:35 +0800

    fixs #1 init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  created () {
+    // 调用API从本地缓存中获取数据
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    console.log('app created and cache logs by setStorageSync')
+  }
+}
+</script>
+
+<style>
+  @font-face {
+    font-family: 'iconfont';  /* project id 744971 */
+    src: url('//at.alicdn.com/t/font_744971_imgaks5rqj.eot');
+    src: url('//at.alicdn.com/t/font_744971_imgaks5rqj.eot?#iefix') format('embedded-opentype'),
+    url('//at.alicdn.com/t/font_744971_imgaks5rqj.woff') format('woff'),
+    url('//at.alicdn.com/t/font_744971_imgaks5rqj.ttf') format('truetype'),
+    url('//at.alicdn.com/t/font_744971_imgaks5rqj.svg#iconfont') format('svg');
+  }
+  .iconfont {
+    font-family:'iconfont';
+    font-size:16px;
+    font-style: normal;
+    color: #9e9595;
+  }
+  page{
+    height: 100%;
+  }
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 200rpx 0;
+  box-sizing: border-box;
+}
+/* this rule will be remove */
+* {
+  transition: width 2s;
+  -moz-transition: width 2s;
+  -webkit-transition: width 2s;
+  -o-transition: width 2s;
+}
+</style>
---
+++ src/data/menu.js
@@ -0,0 +1,198 @@
+const menu = [
+  {
+    id: 1,
+    name: '热销',
+    type: 1,
+    desc: '大家喜欢吃，才叫真好吃。',
+    children: [
+      {
+        name: '方便面',
+        img: '//fuss10.elemecdn.com/2/71/bc2cb7ce587d61b90174b5f89e171jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: '蔬菜',
+    type: 0,
+    desc: '大家喜欢吃，才叫真好吃。',
+    children: [
+      {
+        name: '黄芽菜',
+        img: '//fuss10.elemecdn.com/e/b9/57d4dd59bddff7083def5d7551612png.png?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '花生',
+        img: '//fuss10.elemecdn.com/e/65/1fa36da37a46d4c715089f7bbe6eapng.png?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '胖椒',
+        img: '//fuss10.elemecdn.com/1/5f/aebdd0cfe720359f932a86aa8f7bbjpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '老南京瓜',
+        img: '//fuss10.elemecdn.com/f/2a/449bdd5ea961e8d270a47833a791ejpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '韭黄',
+        img: '//fuss10.elemecdn.com/c/e4/21d85ed2bfa6a51ba13fcda9cd205jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      }
+
+    ]
+  },
+  {
+    id: 3,
+    name: '肉类',
+    type: 0,
+    desc: '大家喜欢吃，才叫真好吃。',
+    children: [
+      {
+        name: '牛腿肉',
+        img: '//fuss10.elemecdn.com/b/9b/5c01c651b6acb55de8ce39b3796afjpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '鸡翅中',
+        img: '//fuss10.elemecdn.com/c/18/97a25df45f531f21b084f9a6632f6jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '广式香肠',
+        img: '//fuss10.elemecdn.com/b/75/b544a21e302973af7e7010605f866jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '玉米肠',
+        img: '//fuss10.elemecdn.com/4/40/5c177a6606af26a434052b2b62538jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '火腿肠',
+        img: '//fuss10.elemecdn.com/9/44/82dadce3b7dc29ea3a45739f647c4jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '皮蛋',
+        img: '//fuss10.elemecdn.com/0/04/b56246cd7c9610e19944e15a89076jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '鸡蛋',
+        img: '//fuss10.elemecdn.com/b/a2/5b8e2f25e32828e82fb4ea7bb0fedjpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '牛百叶',
+        img: '//fuss10.elemecdn.com/f/52/0aa65dceb4bd7fbfcc763503020c3jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '五花肉',
+        img: '//fuss10.elemecdn.com/0/16/12396f884d5f70db5cee3a3c52368jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '猪脑',
+        img: '//fuss10.elemecdn.com/b/ec/d29b1009b2485737688cb637bb8d2jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '肥牛',
+        img: '//fuss10.elemecdn.com/e/c7/a18685d6dbc6ffdd1b740b1d81cd7jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: '鱼类',
+    type: 0,
+    desc: '大家喜欢吃，才叫真好吃。',
+    children: [
+      {
+        name: '扒皮鱼',
+        img: '//fuss10.elemecdn.com/f/9d/8c138439ea3008fc3bd24c315b937jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '小黄鱼',
+        img: '//fuss10.elemecdn.com/a/c3/d8dccd51fa53125c749d9ca69ec08jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '黑鱼',
+        img: '//fuss10.elemecdn.com/9/85/a5144cca12a1d11edf47e825e800ajpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '鱿鱼须',
+        img: '//fuss10.elemecdn.com/9/04/39f419d7c0af4fd6de1cb56df8284jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: '面类',
+    type: 0,
+    desc: '大家喜欢吃，才叫真好吃。',
+    children: [
+      {
+        name: '方便面',
+        img: '//fuss10.elemecdn.com/2/71/bc2cb7ce587d61b90174b5f89e171jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      },
+      {
+        name: '千涨包',
+        img: '//fuss10.elemecdn.com/2/f2/4d5e68d0c9763a0b4aac9e2388627jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      }
+
+    ]
+  },
+  {
+    id: 6,
+    name: '调料',
+    type: 0,
+    desc: '大家喜欢吃，才叫真好吃。',
+    children: [
+      {
+        name: '方便面',
+        img: '//fuss10.elemecdn.com/2/71/bc2cb7ce587d61b90174b5f89e171jpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/',
+        unit: '斤',
+        sales: 400
+      }
+    ]
+  }
+]
+
+export default menu
